ebffc9d88c9484e5afa4121c37c40dd22eee1016
silverup <33813290+YoBella@users.noreply.github.com>
2021-01-19 19:23:07 +0100

    ff

---
+++ Assets/Scripts/TheGame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGame : MonoBehaviour
+{
+    public bool WoodQuestDone = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(WoodQuestDone)
+        {
+            GameObject[] Woodies = GameObject.FindGameObjectsWithTag("Woodman");
+            foreach(GameObject woodie in Woodies)
+            {
+                Destroy(woodie);
+            }
+            GameObject[] WoodiesActive = GameObject.FindGameObjectsWithTag("WoodmanActive");
+            foreach(GameObject actwoodie in WoodiesActive)
+            {
+                actwoodie.GetComponent<BoxCollider2D>().enabled = true;
+                actwoodie.GetComponent<Renderer>().enabled = true;
+            }
+        }
+    }
+}
