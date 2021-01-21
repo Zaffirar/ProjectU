dae91652cec8a0d57f4bc5bf9fc8df4e4c0a9fce
silverup <33813290+YoBella@users.noreply.github.com>
2021-01-21 06:32:25 +0100

    dialogues are finished

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -8,6 +8,16 @@ public class Dialogue
     public string name;
 
     [TextArea(3,10)]
-    public string[] sentences;
+    public string[] sentences0;
+    [TextArea(3, 10)]
+    public string[] sentences1;
+    [TextArea(3, 10)]
+    public string[] sentences2;
+    [TextArea(3, 10)]
+    public string[] sentences3;
+    [TextArea(3, 10)]
+    public string[] sentences4;
+    [TextArea(3, 10)]
+    public string[] sentences5;
 
 }
